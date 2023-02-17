e89d6c7020fb0ffbbddecb0e47d4ec16ab0509a8
beekley <brettbeekley@gmail.com>
2023-02-17 15:02:03 -0800

    Empty vue+router

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <main class="section is-medium">
+        <RouterView :key="$route.path" />
+    </main>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>hello world</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "HomeView",
+    components: {},
+    data() {
+        return {};
+    },
+});
+</script>
