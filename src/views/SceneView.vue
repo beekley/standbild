c7bcf0f7fc0adf47c2e70a262be59a3742ccdd30
beekley <brettbeekley@gmail.com>
2023-02-18 09:25:34 -0800

    Dynamically load scene

---
+++ src/views/SceneView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{ sceneId }}
+        <img :src="`/scenes/${sceneId}.JPG`" usemap="#image-map" />
+        <div v-html="sceneHtml"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "SceneView",
+    components: {},
+    data() {
+        return {
+            sceneId: this.$route.params.id,
+            sceneHtml: "",
+        };
+    },
+    // Get scene HTML.
+    async created() {
+        const resp = await fetch(`/scenes/${this.sceneId}.html`);
+        const html = await resp.text();
+        console.log(html);
+        this.sceneHtml = html;
+    },
+});
+</script>
